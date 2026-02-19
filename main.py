06db67eb58c0d9804b72eb524472ee8c8a629807
srujalyt <srujalhawelikar07@gmail.com>
2026-02-19 18:59:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # AIM: Create a personalized greeting message
 #      User enters name: eg: Shibu. and the program should print "Hello, Shibu" 
-# Coder: 
-# Date:
+# Coder: Srujal Sunil Hawelikar
+# Date: 18/02/2026
 
 # Write your code here
+name=input("Enter  the user name")
+print("Hello", name ) 
