3a786ef590d241e48e558a5bd29fe21af913fc1f
srujalyt <srujalhawelikar07@gmail.com>
2026-02-19 20:32:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 #      User enters name: eg: Shibu. and the program should print "Hello, Shibu" 
 # Coder: Srujal Sunil Hawelikar
 # Date: 18/02/2026
-print("Welcome to Rizvi College of engineering")
-name=input("Enter  the user name")
-print("Welcome" , name  )  
+name=input("Enter name:")
+age=input("Enter age")
+print(f"""Welcome to Rizvi College of Engineering
+Hello, {name}
+You are {age} years old.""")
